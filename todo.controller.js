d0d16abbbcf87c5b7cd565065e4176e4f5a4e3a7
Jiuyang Zhang <Alan@Alans-iMac.local>
2020-06-25 15:39:50 +0900

    use

--- todo.controller.js
+++ todo.controller.js
@@ -6,16 +6,14 @@ app.controller("myCtrl", function($scope) {
       $scope.todoList.push(next);
     }
   }
-  $scope.addDone = function(item) {
-    var index = $scope.todoList.indexOf(item);
-    if (index >= 0) {
+  $scope.addDone = function(item, index) {
+    if ($scope.todoList.includes(item)) {
       $scope.todoList.splice(index, 1);
       $scope.doneList.push(item);
     }
   }
-  $scope.undone = function(item) {
-    var index = $scope.doneList.indexOf(item);
-    if (index >= 0) {
+  $scope.undone = function(item, index) {
+    if ($scope.doneList.includes(item)) {
       $scope.doneList.splice(index, 1);
       $scope.todoList.push(item);
     }
